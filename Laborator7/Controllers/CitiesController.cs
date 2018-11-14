6c4b1fc73457b4ad5160f1919a370eb3a4265155
andreicostandache <andrei97mihai@gmail.com>
2018-11-14 11:07:08 +0200

    changes

--- Laborator7/Controllers/CitiesController.cs
+++ Laborator7/Controllers/CitiesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BusinessLayer;
+using DataLayer;
 
 namespace Laborator7.Controllers
 {
@@ -11,5 +13,6 @@ namespace Laborator7.Controllers
     [ApiController]
     public class CitiesController : ControllerBase
     {
+        private readonly ICityRepository _cityRepository;
     }
 }
